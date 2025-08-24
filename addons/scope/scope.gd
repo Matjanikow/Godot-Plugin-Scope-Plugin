c1cb59a86e92ad043304ddf51d49c2866de05899
Matata <matjanikow@gmail.com>
2025-08-24 18:34:01 -0300

    Fix Scope para windows en 4.4

--- addons/scope/scope.gd
+++ addons/scope/scope.gd
@@ -1,15 +1,25 @@
 @tool
 extends EditorPlugin
 const SCOPE = preload("res://addons/scope/scopeButton.tscn")
+#var dock = "@Panel@14/@VBoxContainer@15/DockHSplitLeftL/DockHSplitLeftR/
+#DockVSplitLeftR/DockSlotLeftUR/Scene/@HBoxContainer@5046"
 var scope : Control
 var menuPanel : Node
+var scene_dock : Control
+
 func _enter_tree():
 	scope = SCOPE.instantiate()
-	var sceneParent = get_tree().root.find_child("Scene", true, false)
+	#var sceneParent = get_tree().root.find_child("Scene", true, false)
+	var sceneParent = null
+	for c in get_tree().root.find_children("Scene", "", true, false):
+		if c is VBoxContainer: 
+			sceneParent = c
+			break
 	menuPanel = sceneParent.find_child("@HBox*", false, false)
 	menuPanel.add_child(scope)
 	menuPanel.move_child(scope, menuPanel.get_child_count()-2)
-
+	
 func _exit_tree():
-	menuPanel.remove_child(scope)
+	#menuPanel.remove_child(scope)
+	remove_control_from_container(EditorPlugin.CONTAINER_PROJECT_SETTING_TAB_LEFT, scope)
 	scope.queue_free()
--- addons/scope/scopeButton.gd
+++ addons/scope/scopeButton.gd
@@ -3,7 +3,11 @@ extends Control
 
 func _on_scope_button_pressed():
 	#print(EditorInterface.get_selection().get_selected_nodes()[0].get_path())
-	var sceneParent = get_tree().root.find_child("Scene", true, false)
+	var sceneParent = null
+	for c in get_tree().root.find_children("Scene", "", true, false):
+		if c is VBoxContainer: 
+			sceneParent = c
+			break
 	var sceneEditor = sceneParent.find_child("*SceneTreeEditor*", false, false)
 	var tree:Tree = sceneEditor.find_child("*Tree*", false, false)
 	if tree.get_selected() != null:
