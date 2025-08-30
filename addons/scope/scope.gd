3116d9f9084528e19f41471f0409f4271bb57865
Julian Zaltron <zaltron.julian@gmail.com>
2025-08-30 00:12:16 -0300

    feat: identifies light or dark mode and picks an svg accordingly

--- addons/scope/scope.gd
+++ addons/scope/scope.gd
@@ -1,13 +1,21 @@
 @tool
 extends EditorPlugin
-const SCOPE = preload("res://addons/scope/scopeButton.tscn")
+var editorSettings = get_editor_interface().get_editor_settings()
+var baseColor = editorSettings.get_setting("interface/theme/base_color")
+var baseColorLuminance = baseColor.srgb_to_linear().get_luminance()
+var lightMode = baseColorLuminance > 0.5
+var scopePath = "res://addons/scope/scopeButtonLightMode.tscn" if lightMode else "res://addons/scope/scopeButton.tscn"
+var SCOPE = load(scopePath)
+
 #var dock = "@Panel@14/@VBoxContainer@15/DockHSplitLeftL/DockHSplitLeftR/
 #DockVSplitLeftR/DockSlotLeftUR/Scene/@HBoxContainer@5046"
 var scope : Control
 var menuPanel : Node
-var scene_dock : Control
 
 func _enter_tree():
+	print(baseColorLuminance)
+	print(scopePath)
+	print(lightMode)
 	scope = SCOPE.instantiate()
 	#var sceneParent = get_tree().root.find_child("Scene", true, false)
 	var sceneParent = null
@@ -16,6 +24,7 @@ func _enter_tree():
 			sceneParent = c
 			break
 	menuPanel = sceneParent.find_child("@HBox*", false, false)
+	
 	menuPanel.add_child(scope)
 	menuPanel.move_child(scope, menuPanel.get_child_count()-2)
 	
