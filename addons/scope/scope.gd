75f4e2c8a5aaaac694b8451edf134656a6a211e3
Julian Zaltron <zaltron.julian@gmail.com>
2025-08-31 03:05:16 -0300

    feat: button changes upon theme change real time 100% true no fake

--- addons/scope/scope.gd
+++ addons/scope/scope.gd
@@ -1,11 +1,5 @@
 @tool
 extends EditorPlugin
-var editorSettings = get_editor_interface().get_editor_settings()
-var baseColor = editorSettings.get_setting("interface/theme/base_color")
-var baseColorLuminance = baseColor.srgb_to_linear().get_luminance()
-var lightMode = baseColorLuminance > 0.5
-var scopePath = "res://addons/scope/scopeButtonLightMode.tscn" if lightMode else "res://addons/scope/scopeButton.tscn"
-var SCOPE = load(scopePath)
 
 #var dock = "@Panel@14/@VBoxContainer@15/DockHSplitLeftL/DockHSplitLeftR/
 #DockVSplitLeftR/DockSlotLeftUR/Scene/@HBoxContainer@5046"
@@ -13,18 +7,36 @@ var scope : Control
 var menuPanel : Node
 
 func _enter_tree():
+	_set_scope()
+	get_tree().root.theme_changed.connect(_theme_changed)
+
+func _set_scope():
+	var editorSettings = get_editor_interface().get_editor_settings()
+	var baseColor = editorSettings.get_setting("interface/theme/base_color")
+	var baseColorLuminance = baseColor.srgb_to_linear().get_luminance()
+	var lightMode = baseColorLuminance > 0.5
+	var scopePath = "res://addons/scope/scopeButtonLightMode.tscn" if lightMode else "res://addons/scope/scopeButton.tscn"
+	var SCOPE = load(scopePath)
 	scope = SCOPE.instantiate()
-	#var sceneParent = get_tree().root.find_child("Scene", true, false)
+	_add_scope()
+	
+func _add_scope():
 	var sceneParent = null
 	for c in get_tree().root.find_children("Scene", "", true, false):
 		if c is VBoxContainer: 
 			sceneParent = c
 			break
 	menuPanel = sceneParent.find_child("@HBox*", false, false)
-	
 	menuPanel.add_child(scope)
 	menuPanel.move_child(scope, menuPanel.get_child_count()-2)
-	
-func _exit_tree():
+
+func _theme_changed():
+	_delete_scope()
+	_set_scope()
+
+func _delete_scope():
 	menuPanel.remove_child(scope)
 	scope.queue_free()
+	
+func _exit_tree():
+	_delete_scope()
